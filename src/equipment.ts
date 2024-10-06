5bcbc6024bdeaf1ec04e3f3004b205fe8a89c3e7
Karnnitra Sukpanich <karnnitra.s@ku.th>
2024-10-06 22:46:57 +0700

    Add: query

--- src/equipment.ts
+++ src/equipment.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import db from "./db";
 import { password } from "bun";
 
@@ -21,16 +21,30 @@ app.get("/searchbyName/:equipment_name", async ({params}) => {
   return await db.$queryRaw`SELECT staff_id, "username", staff_name, birthday, gender, staff_phone_number, role, password, email FROM "staff" WHERE LOWER(replace("equipment_name",' ','')) like '%' || ${params.equipment_name} || '%';;`;
 });
 
-app.patch("/updateAmount/:equipment_id", async ({ params, body }) => {
-  const { amount } = body as { amount: number }; 
-  const newAmount = Number(amount);  
-  if (isNaN(newAmount) || newAmount < 0) {
-      return { error: "Invalid amount" };
-  }
-  await db.$executeRaw`UPDATE "equipment" SET "amount" = ${newAmount} WHERE "equipment_id" = ${Number(params.equipment_id)};`;
+app.post(
+  "/editAmount/:equipment_id",
+  async ({ body }) => {
+    const { equipment_id, amount } = body;
 
-  return { success: true, message: `Updated amount for equipment ID ${Number(params.equipment_id)} to ${newAmount}` };
-});//not work
+    const result = await db.$queryRaw`
+      UPDATE "equipment"
+      SET amount = ${amount}
+      WHERE "equipment_id" = ${equipment_id};
+    `;
+
+    return {
+      success: true,
+      message: "Amount updated successfully",
+    };
+  },
+  {
+    body: t.Object({
+      equipment_id: t.Integer({ minimum: 0, maximum: 9999 }),
+      amount: t.Integer({ minimum: 0 }),
+    }),
+  }
+);
+
 
 
 
--- src/patient.ts
+++ src/patient.ts
@@ -1,39 +1,150 @@
-import { Elysia,t } from "elysia";
+import { Elysia, t } from "elysia";
 import db from "./db";
 import { password } from "bun";
 
-const app = new Elysia({ prefix: "/patient", detail: {tags: ["Patient"]} });
+const app = new Elysia({ prefix: "/patient", detail: { tags: ["Patient"] } });
 
-app.get("/searchbyID/:patient_id", async ({params}) => {
-  return await db.$queryRaw`SELECT patient_id, name, phone_number, birthday, gender, appoinment_date, course_count FROM "patient" WHERE "patient_id" = ${Number(params.patient_id)} Limit 1;`;
+app.get("/searchbyID/:patient_id", async ({ params }) => {
+  return await db.$queryRaw`SELECT patient_id, name_surname, phone_number, birthday, gender, appointment_date, course_count, first_visit_date FROM "patient" WHERE "patient_id" = ${Number(
+    params.patient_id
+  )} Limit 1;`;
 });
 
-app.get("/searchbyName/:name", async ({params}) => {
-  return await db.$queryRaw`SELECT patient_id, name, phone_number, birthday, gender, appoinment_date, course_count FROM "patient" WHERE LOWER(replace("name", ' ', '')) LIKE '%' || LOWER(${params.name}) || '%'; `;
+app.get("/searchbyName/:name_surname", async ({ params }) => {
+  return await db.$queryRaw`SELECT patient_id, name_surname, phone_number, birthday, gender, appointment_date, course_count, first_visit_date FROM "patient" WHERE LOWER(replace("name_surname", ' ', '')) LIKE '%' || LOWER(${params.name_surname}) || '%'; `;
 });
-
 
 app.get("/getPatientList", async () => {
-  return await db.$queryRaw`SELECT patient_id, name, phone_number, birthday, gender, appoinment_date, course_count FROM "patient";`;
+  return await db.$queryRaw`SELECT patient_id, name_surname, phone_number, birthday, gender, appointment_date, course_count, first_visit_date FROM "patient";`;
 });
 
 app.get("/getNamebyID/:patient_id", async ({ params }) => {
-  return await db.$queryRaw`SELECT name FROM "patient" WHERE "patient_id" = ${Number(params.patient_id)} LIMIT 1;`;
+  return await db.$queryRaw`SELECT name_surname FROM "patient" WHERE "patient_id" = ${Number(
+    params.patient_id
+  )} LIMIT 1;`;
 });
 
-app.post("/addPatient", async ({body}) => {
-  const { name, phone_number, birthday, gender, appoinment_date, course_count } = body;
+app.post(
+  "/addPatient",
+  async ({ body }) => {
+    const {
+      name_surname,
+      phone_number,
+      birthday,
+      gender,
+      appointment_date,
+      course_count,
+      first_visit_date,
+    } = body;
 
-  const result = await db.$queryRaw`
+    const result = await db.$queryRaw`
     INSERT INTO "patient" 
-    ( "name", phone_number, birthday, gender, appoinment_date, course_count) 
-    VALUES (${name}, ${phone_number}, ${birthday}, ${gender}, ${appoinment_date}, ${course_count});
+    ( "name_surname", phone_number, birthday, gender, appointment_date, course_count, first_visit_date) 
+    VALUES (${name_surname}, ${phone_number}, ${birthday}, ${gender}, ${appointment_date}, ${course_count}, ${first_visit_date});
   `;
-  
-  return { success: true, message: "Patient added successfully", result };
-},{body:t.Object({
-  name:t.String({minLength:1, maxLength:50}), phone_number:t.String({minLength:10, maxLength:10}), birthday:t.Date(), gender:t.String({minLength:1, maxLength:10}), appoinment_date:t.Date(), course_count:t.Integer({minimum: 0, maximum: 10}) 
-})});
+
+    return { success: true, message: "Patient added successfully", result };
+  },
+  {
+    body: t.Object({
+      name_surname: t.String({ minLength: 1, maxLength: 50 }),
+      phone_number: t.String({ minLength: 10, maxLength: 10 }),
+      birthday: t.Date(),
+      gender: t.String({ minLength: 1, maxLength: 10 }),
+      appointment_date: t.Date(),
+      course_count: t.Integer({ minimum: 0, maximum: 10 }),
+      first_visit_date: t.Date(),
+    }),
+  }
+);
+
+app.post(
+  "/editPatient/:patient_id",
+  async ({ body }) => {
+    const {
+      patient_id,
+      name_surname,
+      phone_number,
+      birthday,
+      gender,
+      appointment_date,
+      course_count,
+      first_visit_date,
+    } = body;
+
+    const result = await db.$queryRaw`
+    UPDATE "patient"  SET
+    "name_surname" = ${name_surname} , phone_number = ${phone_number}, birthday = ${birthday}, gender = ${gender}, appointment_date = ${appointment_date}, course_count = ${course_count}, first_visit_date = ${first_visit_date}
+    WHERE "patient_id"  = ${patient_id}
+  `;
+    return "Work";
+  },
+  {
+    body: t.Object({
+      patient_id: t.Integer({ minimum: 0, maximum: 9999 }),
+      name_surname: t.String({ minLength: 1, maxLength: 50 }),
+      phone_number: t.String({ minLength: 10, maxLength: 10 }),
+      birthday: t.Date(),
+      gender: t.String({ minLength: 1, maxLength: 10 }),
+      appointment_date: t.Date(),
+      course_count: t.Integer({ minimum: 0, maximum: 10 }),
+      first_visit_date: t.Date(),
+    }),
+  }
+);
+
+app.post(
+  "/editAppoinmentDate/:patient_id",
+  async ({ body }) => {
+    const { patient_id, appointment_date } = body;
+
+    const result = await db.$queryRaw`
+      UPDATE "patient"
+      SET appointment_date = ${appointment_date}
+      WHERE "patient_id" = ${patient_id};
+    `;
+
+    return {
+      success: true,
+      message: "Appointment Date updated successfully",
+    };
+  },
+  {
+    body: t.Object({
+      patient_id: t.Integer({ minimum: 0, maximum: 9999 }),
+      appointment_date: t.Date(),
+    }),
+  }
+);
+
+interface PatientCountResult {
+  patient_count: number;  
+}
+
+
+app.post(
+  "/countByDate",
+  async ({ body }) => {
+    const { appointment_date } = body;
+
+    const result = await db.$queryRaw<PatientCountResult[]>`
+      SELECT COUNT(appointment_date) as patient_count
+      FROM "patient"
+      WHERE "appointment_date"::date = ${appointment_date}::date;
+    `;
+
+    return {
+      success: true,
+      count: result[0].patient_count.toString(),
+    };
+  },
+  {
+    body: t.Object({
+      appointment_date: t.String(), 
+    }),
+  }
+);
+
 
 
 export default app;
