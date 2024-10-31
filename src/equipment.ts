17821eab4d1183a6d2b79f7afd4deabdb60ae091
Karnnitra Sukpanich <karnnitra.s@ku.th>
2024-10-31 20:41:26 +0700

    Add: query in equipment and patient

--- src/equipment.ts
+++ src/equipment.ts
@@ -90,4 +90,56 @@ app.post(
   }
 );
 
+app.post(
+  "/decreaseEquipment",
+  async ({ body }: { body: Equipment }) => {
+    try {
+      const {
+        equipment_id,
+        equipment_name,
+        price,
+        amount,
+      } = body;
+
+      const existingEquipment = await db.$queryRaw<ExistingEquipment[]>`
+        SELECT "amount" FROM "equipment" WHERE "equipment_id" = ${equipment_id};
+      `;
+
+      if (existingEquipment.length === 0) {
+        return {
+          success: false,
+          error: "Equipment not found",
+        };
+      }
+      
+      const newAmount = existingEquipment[0].amount - amount; 
+
+      await db.$queryRaw`
+        UPDATE "equipment"
+        SET
+          "equipment_name" = ${equipment_name},
+          "price" = ${price},
+          "amount" = ${newAmount}
+        WHERE "equipment_id" = ${equipment_id};
+      `;
+
+      return { success: true, message: "Equipment updated successfully" };
+    } catch (error: any) {
+      return {
+        success: false,
+        error: "Error while updating equipment data",
+        details: error.message,
+      };
+    }
+  },
+  {
+    body: t.Object({
+      equipment_id: t.Integer({ minimum: 0 }),
+      equipment_name: t.String({ minLength: 1, maxLength: 100 }),
+      price: t.Number({ minimum: 0 }),
+      amount: t.Integer({ minimum: 0 }), 
+    }),
+  }
+);
+
 export default app;
